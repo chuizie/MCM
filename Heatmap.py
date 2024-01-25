3ef103cce421aceef5aa698a806641a47d8f8703
Nygaard <148783594+chuizie@users.noreply.github.com>
2024-01-25 22:20:53 +0800

    Create Heatmap.py

---
+++ Heatmap.py
@@ -0,0 +1,13 @@
+# library
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create a dataset
+df = pd.DataFrame(np.random.random((5,5)), columns=["a","b","c","d","e"])
+
+# Default heatmap
+p1 = sns.heatmap(df)
+
+plt.show()
